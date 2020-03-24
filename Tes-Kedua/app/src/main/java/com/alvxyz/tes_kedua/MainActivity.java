efa8aeeb0737b3540d4beeabf3f45f2de7116a77
alvxyz <alvianteddy@gmail.com>
2020-03-24 07:44:21 +0700

    Upload ke Git folder Pemrograman Mobile - 4

---
+++ Tes-Kedua/app/src/main/java/com/alvxyz/tes_kedua/MainActivity.java
@@ -0,0 +1,14 @@
+package com.alvxyz.tes_kedua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
